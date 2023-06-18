d0314c1774802dca89c357900295492d2714b1f8
Substrata <digital_strata@mail.ru>
2023-06-18 18:33:12 +0300

    added solution for task60(3D massive)

--- task60/Program.cs
+++ task60/Program.cs
@@ -4,3 +4,56 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+
+void PrintArray(int[,,] array)
+{
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                Console.Write($"{array[x,y,z]}({x},{y},{z}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+int[,,] FillRandom()
+{
+    Random random = new Random();
+    int[,,] array3D = new int[2,2,2];
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {                            
+                int randNumber = random.Next(10, 100);
+                while (Array3DContains(array3D, randNumber))
+                {
+                    randNumber = random.Next(10, 100);
+                }
+            array3D[i, j, k] = randNumber;
+            }
+        }
+    }
+    return array3D;
+}
+bool Array3DContains(int[,,] array3D, int randNumber)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                if (array3D[i, j, k] == randNumber)
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int[,,] newArray3D = FillRandom();
+PrintArray(newArray3D);
